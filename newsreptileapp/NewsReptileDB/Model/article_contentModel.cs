e73cdaeb731818abf6254b9a699172d9dd3f49cb
HuangMinQiang <1018720141@qq.com>
2019-09-26 09:59:52 +0800

    添加注释

--- newsreptileapp/NewsReptileDB/Model/article_contentModel.cs
+++ newsreptileapp/NewsReptileDB/Model/article_contentModel.cs
@@ -9,13 +9,25 @@ namespace NewsReptileDB.DB.Model
     public class article_contentModel
     {
         public Int32? id { get; set; }
+        /// <summary>
+        /// 网站数据表id
+        /// </summary>
         public Int32? web_data_id { get; set; }
+        /// <summary>
+        /// 数据
+        /// </summary>
         public String data { get; set; }
+        /// <summary>
+        /// 原始数据（暂时没用）
+        /// </summary>
         public String data_original { get; set; }
         /// <summary>
         /// 0为原始  1为文本
         /// </summary>
         public Int32? _type { get; set; }
+        /// <summary>
+        /// 数据索引
+        /// </summary>
         public Int32? _index { get; set; }
     }
 }
