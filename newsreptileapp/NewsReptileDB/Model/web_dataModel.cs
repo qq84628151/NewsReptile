327e4b767da0cf2d0ddde5ca11e04355055824a1
HuangMinQiang <1018720141@qq.com>
2019-09-26 09:58:47 +0800

    添加注释

--- newsreptileapp/NewsReptileDB/Model/web_dataModel.cs
+++ newsreptileapp/NewsReptileDB/Model/web_dataModel.cs
@@ -9,19 +9,49 @@ namespace NewsReptileDB.DB.Model
     public class web_dataModel
     {
         public Int32? id { get; set; }
+        /// <summary>
+        /// 爬取来源
+        /// </summary>
         public String source { get; set; }
+        /// <summary>
+        /// 文章来源
+        /// </summary>
         public String source2 { get; set; }
+        /// <summary>
+        /// 开始标题
+        /// </summary>
         public String top_title { get; set; }
+        /// <summary>
+        /// 开始标题的html数据
+        /// </summary>
         public String toptitle_original { get; set; }
+        /// <summary>
+        /// 文章标题
+        /// </summary>
         public String title { get; set; }
+        /// <summary>
+        /// 文章标题html数据
+        /// </summary>
         public String title_original { get; set; }
+        /// <summary>
+        /// 文章地址
+        /// </summary>
         public String url { get; set; }
+        /// <summary>
+        /// 文章发布时间
+        /// </summary>
         public DateTime? article_time { get; set; }
+        /// <summary>
+        /// 文章图片路径
+        /// </summary>
         public String img_path { get; set; }
         /// <summary>
         /// 1为特殊类型，内容为原始带标签
         /// </summary>
         public Int32? _type { get; set; }
+        /// <summary>
+        /// 文章描述
+        /// </summary>
         public String descript { get; set; }
         public DateTime? create_datetime { get; set; }
         public Boolean? del { get; set; }
--- newsreptileapp/NewsReptileDB/Model/web_imgModel.cs
+++ newsreptileapp/NewsReptileDB/Model/web_imgModel.cs
@@ -9,8 +9,17 @@ namespace NewsReptileDB.DB.Model
     public class web_imgModel
     {
         public Int32? id { get; set; }
+        /// <summary>
+        /// 图片本地路径
+        /// </summary>
         public String img_path { get; set; }
+        /// <summary>
+        /// 网站数据表id
+        /// </summary>
         public Int32? web_data_id { get; set; }
+        /// <summary>
+        /// 图片地址
+        /// </summary>
         public String url { get; set; }
         public DateTime? create_datetime { get; set; }
         public Boolean? del { get; set; }
